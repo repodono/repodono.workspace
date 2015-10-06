ef520840fe954414c6c4bb7ca4f43eb05a2f16cb
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-10-06 17:38:25 +1300

    Turn testing into its own namespace
    - With layers moved to its own module.

---
+++ src/repodono/workspace/testing/__init__.py
@@ -0,0 +1,8 @@
+from repodono.workspace.testing.layers import (
+        RepodonoWorkspaceLayer,
+
+        REPODONO_WORKSPACE_FIXTURE,
+        REPODONO_WORKSPACE_INTEGRATION_TESTING,
+        REPODONO_WORKSPACE_FUNCTIONAL_TESTING,
+        REPODONO_WORKSPACE_ACCEPTANCE_TESTING,
+    )
