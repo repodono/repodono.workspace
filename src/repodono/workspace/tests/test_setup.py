cc5d601c2f2e8c196142d31d08764797a9c50d0f
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-11-05 11:54:59 +1300

    PEP8.

--- src/repodono/workspace/tests/test_setup.py
+++ src/repodono/workspace/tests/test_setup.py
@@ -17,8 +17,11 @@ class TestSetup(unittest.TestCase):
         self.installer = api.portal.get_tool('portal_quickinstaller')
 
     def test_product_installed(self):
-        """Test if repodono.workspace is installed with portal_quickinstaller."""
-        self.assertTrue(self.installer.isProductInstalled('repodono.workspace'))
+        """
+        Test if repodono.workspace is installed with portal_quickinstaller.
+        """
+        self.assertTrue(
+            self.installer.isProductInstalled('repodono.workspace'))
 
     def test_browserlayer(self):
         """Test that IRepodonoWorkspaceLayer is registered."""
@@ -38,7 +41,8 @@ class TestUninstall(unittest.TestCase):
 
     def test_product_uninstalled(self):
         """Test if repodono.workspace is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled('repodono.workspace'))
+        self.assertFalse(self.installer.isProductInstalled(
+            'repodono.workspace'))
 
     def test_browserlayer_removed(self):
         """Test that IRepodonoWorkspaceLayer is removed."""
